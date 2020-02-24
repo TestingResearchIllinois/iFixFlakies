b31c5417109a56c4562a0b7636eed83c60e125f6
winglam <wing.c.lam@gmail.com>
2020-02-24 14:24:19 -0600

    Adding feature to allow Minimizer to look for polluters and cleaners from the original order instead of just the passing and failing order.

--- src/main/java/edu/illinois/cs/dt/tools/minimizer/MinimizerPlugin.java
+++ src/main/java/edu/illinois/cs/dt/tools/minimizer/MinimizerPlugin.java
@@ -2,28 +2,36 @@ package edu.illinois.cs.dt.tools.minimizer;
 
 import com.reedoei.eunomia.collections.StreamUtil;
 import edu.illinois.cs.dt.tools.detection.DetectorPathManager;
+import edu.illinois.cs.dt.tools.detection.DetectorPlugin;
+import edu.illinois.cs.dt.tools.detection.detectors.Detector;
+import edu.illinois.cs.dt.tools.detection.detectors.RandomDetector;
 import edu.illinois.cs.dt.tools.runner.InstrumentingSmartRunner;
 import edu.illinois.cs.dt.tools.runner.data.DependentTest;
 import edu.illinois.cs.dt.tools.runner.data.DependentTestList;
-import edu.illinois.cs.dt.tools.runner.data.TestRun;
+import edu.illinois.cs.dt.tools.utility.ErrorLogger;
 import edu.illinois.cs.testrunner.configuration.Configuration;
-import edu.illinois.cs.testrunner.data.results.Result;
 import edu.illinois.cs.testrunner.mavenplugin.TestPlugin;
 import edu.illinois.cs.testrunner.mavenplugin.TestPluginPlugin;
-import edu.illinois.cs.testrunner.runner.Runner;
 import edu.illinois.cs.testrunner.runner.RunnerFactory;
+import edu.illinois.cs.dt.tools.runner.data.TestRun;
+import edu.illinois.cs.testrunner.data.results.Result;
+import edu.illinois.cs.testrunner.runner.Runner;
 import org.apache.maven.project.MavenProject;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
+import java.util.Collections;
 
 public class MinimizerPlugin extends TestPlugin {
     private TestMinimizerBuilder builder;
     private InstrumentingSmartRunner runner;
-
+    private final String TEST_TO_MINIMIZE = Configuration.config().getProperty("dt.minimizer.dependent.test", null);
+    private final boolean USE_ORIGINAL_ORDER = Configuration.config().getProperty("dt.minimizer.use.original.order", false);
     private static final boolean VERIFY_DTS = Configuration.config().getProperty("dt.verify", true);
 
     /**
@@ -39,33 +47,55 @@ public class MinimizerPlugin extends TestPlugin {
         this.builder = new TestMinimizerBuilder(runner);
     }
 
-    private Stream<TestMinimizer> fromDtList(final Path path) {
+    private Stream<TestMinimizer> fromDtList(final Path path, MavenProject project) {
         TestPluginPlugin.info("Creating minimizers for file: " + path);
 
         try {
-            final DependentTestList dependentTestList = DependentTestList.fromFile(path);
-            if (dependentTestList == null) {
-                throw new IllegalArgumentException("Dependent test list file is empty");
+            List<String> originalOrder = DetectorPlugin.getOriginalOrder(project);
+
+            if (!Files.exists(DetectorPathManager.originalOrderPath()) || originalOrder.isEmpty()) {
+                TestPluginPlugin.info("Original order file not found or is empty. Creating original-order file now at: "
+                                              + DetectorPathManager.originalOrderPath());
+                originalOrder = DetectorPlugin.getOriginalOrder(project);
+                Files.write(DetectorPathManager.originalOrderPath(), originalOrder);
             }
 
-            return dependentTestList.dts().stream()
-                    .flatMap(dt -> minimizers(dt, builder, runner));
+            DependentTestList dependentTestList = DependentTestList.fromFile(path);
+            if (TEST_TO_MINIMIZE != null) {
+                TestPluginPlugin.info("Filtering dependent test list to run only for: " + TEST_TO_MINIMIZE);
+                Optional<DependentTest> dependentTest = dependentTestList.dts().stream().
+                        filter(dt -> dt.name().equalsIgnoreCase(TEST_TO_MINIMIZE)).findFirst();
+
+                if (!dependentTest.isPresent()) {
+                    throw new IllegalArgumentException("Dependent test name is specificed but could not find matching dependent test.");
+                } else {
+                    return minimizers(dependentTest.get(), builder, runner, USE_ORIGINAL_ORDER ? originalOrder : null);
+                }
+            } else {
+                List<String> finalOriginalOrder = originalOrder;
+                if (dependentTestList.dts().size() > 1) {
+                    TestPluginPlugin.debug("More than one dependent test list detected. Original order cannot be trusted.");
+                }
+                return dependentTestList.dts().stream()
+		    .flatMap(dt -> minimizers(dt, builder, runner, USE_ORIGINAL_ORDER ? finalOriginalOrder : null));
+            }
         } catch (IOException e) {
             return Stream.empty();
         }
     }
 
-    private Stream<TestMinimizer> minimizers(final DependentTest dependentTest,
-            final TestMinimizerBuilder builder, final Runner runner) {
+    public Stream<TestMinimizer> minimizers(final DependentTest dependentTest,
+					    final TestMinimizerBuilder builder,
+					    final Runner runner,
+					    List<String> originalOrder) {
         final TestRun intended = dependentTest.intended();
         final TestRun revealed = dependentTest.revealed();
         final String name = dependentTest.name();
-
         final TestMinimizerBuilder minimizerBuilder = builder.dependentTest(name);
 
         if (VERIFY_DTS) {
             if (!intended.verify(name, runner, null) || !revealed.verify(name, runner, null)) {
-                return Stream.of(minimizerBuilder.buildNOD());
+		return Stream.of(minimizerBuilder.buildNOD());
             }
         }
 
@@ -81,21 +111,43 @@ public class MinimizerPlugin extends TestPlugin {
             }
         }
 
-        if (!isolationResult.equals(Result.PASS)) { // Does not pass in isolation, needs setter, so need to minimize passing order
+        if (originalOrder != null) {
+            TestPluginPlugin.info("Using original order to run Minimizer instead of intended or revealed order.");
+            if (!isolationResult.equals(Result.PASS)) {
+                return Stream.of(minimizerBuilder.testOrder(reorderOriginalOrder(intended.order(), originalOrder)).build());
+            } else {
+                return Stream.of(minimizerBuilder.testOrder(reorderOriginalOrder(revealed.order(), originalOrder)).build());
+            }
+        } else if (!isolationResult.equals(Result.PASS)) { // Does not pass in isolation, needs setter, so need to minimize passing order
             return Stream.of(minimizerBuilder.testOrder(intended.order()).build());
         } else {    // Otherwise passes in isolation, needs polluter, so need to minimize failing order
             return Stream.of(minimizerBuilder.testOrder(revealed.order()).build());
         }
     }
 
-    public Stream<MinimizeTestsResult> runDependentTestFolder(final Path dtFolder) throws IOException {
-        return Files.walk(dtFolder)
-                .filter(p -> Files.isRegularFile(p))
-                .flatMap(this::runDependentTestFile);
+
+    private List<String> reorderOriginalOrder(List<String> intended, List<String> originalOrder) {
+        List<String> retList = new ArrayList<>(intended);
+
+        for (String test : originalOrder) {
+            if (!intended.contains(test)) {
+                retList.add(test);
+            }
+        }
+        try {
+            Files.write(DetectorPathManager.originalOrderPath(), retList);
+        } catch (IOException e) {
+            TestPluginPlugin.error("Created new original order but could not write to "
+                                           + DetectorPathManager.originalOrderPath());
+            return retList;
+        }
+        TestPluginPlugin.info("Reordered original order to have some tests come first. For tests: " + intended);
+
+        return retList;
     }
 
-    public Stream<MinimizeTestsResult> runDependentTestFile(final Path dtFile) {
-        return fromDtList(dtFile).flatMap(minimizer -> {
+    public Stream<MinimizeTestsResult> runDependentTestFile(final Path dtFile, MavenProject project) {
+        return fromDtList(dtFile, project).flatMap(minimizer -> {
             try {
                 final MinimizeTestsResult result = minimizer.get();
                 result.save();
@@ -113,6 +165,6 @@ public class MinimizerPlugin extends TestPlugin {
         this.runner = InstrumentingSmartRunner.fromRunner(RunnerFactory.from(project).get());
         this.builder = new TestMinimizerBuilder(runner);
 
-        StreamUtil.seq(runDependentTestFile(DetectorPathManager.detectionFile()));
+        StreamUtil.seq(runDependentTestFile(DetectorPathManager.detectionFile(), project));
     }
 }
--- src/main/java/edu/illinois/cs/dt/tools/minimizer/TestMinimizer.java
+++ src/main/java/edu/illinois/cs/dt/tools/minimizer/TestMinimizer.java
@@ -31,12 +31,13 @@ public class TestMinimizer extends FileCache<MinimizeTestsResult> {
     protected final Result expected;
     protected final Result isolationResult;
     protected final SmartRunner runner;
+    protected final List<String> fullTestOrder;
+    private final boolean ONE_BY_ONE_POLLUTERS = Configuration.config().getProperty("dt.minimizer.polluters.one_by_one", false);
+    private static final boolean FIND_ALL = Configuration.config().getProperty("dt.find_all", true);
 
     protected final Path path;
 
     protected TestRunResult expectedRun;
-
-    private static final boolean FIND_ALL = Configuration.config().getProperty("dt.find_all", true);
 
     private void debug(final String str) {
         TestPluginPlugin.mojo().getLog().debug(str);
@@ -48,6 +49,7 @@ public class TestMinimizer extends FileCache<MinimizeTestsResult> {
 
     public TestMinimizer(final List<String> testOrder, final SmartRunner runner, final String dependentTest) {
         // Only take the tests that come before the dependent test
+        this.fullTestOrder = testOrder;
         this.testOrder = testOrder.contains(dependentTest) ? ListUtil.before(testOrder, dependentTest) : testOrder;
         this.dependentTest = dependentTest;
 
@@ -86,61 +88,18 @@ public class TestMinimizer extends FileCache<MinimizeTestsResult> {
         return OperationTime.runOperation(() -> {
             info("Running minimizer for: " + dependentTest + " (expected result in this order: " + expected + ")");
 
-            boolean foundFirst = false;
-
-            final List<String> order =
-                    testOrder.contains(dependentTest) ? ListUtil.beforeInc(testOrder, dependentTest) : new ArrayList<>(testOrder);
-
             // Keep going as long as there are tests besides dependent test to run
             List<PolluterData> polluters = new ArrayList<>();
             int index = 0;
-            while (!order.isEmpty()) {
-                // First need to check if remaining tests in order still lead to expected value
-                if (result(order) != expected) {
-                    info("Remaining tests no longer match expected: " + order);
-                    break;
+
+            if (ONE_BY_ONE_POLLUTERS) {
+                info("Getting all polluters (dt.minimizer.polluters.one_by_one is set to true)");
+                for (List<String> order : getSingleTests(fullTestOrder, dependentTest)) {
+                    index = getPolluters(order, startTime, polluters, index);
                 }
-
-                final OperationTime[] operationTime = new OperationTime[1];
-                final List<String> deps = OperationTime.runOperation(() -> {
-                    return run(new ArrayList<>(order));
-                }, (foundDeps, time) -> {
-                    operationTime[0] = time;
-                    return foundDeps;
-                });
-
-                if (deps.isEmpty()) {
-                    info("Did not find any deps");
-                    break;
-                }
-
-                info("Ran minimizer, dependencies: " + deps);
-                double elapsedSeconds = System.currentTimeMillis() / 1000.0 - startTime / 1000.0;
-                if (!foundFirst) {
-                    info("FIRST POLLUTER: Found first polluter " + deps + " for dependent test " + dependentTest + " in " + elapsedSeconds + " seconds.");
-                    foundFirst = true;
-                } else {
-                    info("POLLUTER: Found polluter " + deps + " for dependent test " + dependentTest + " in " + elapsedSeconds + " seconds.");
-                }
-
-
-                // Only look for cleaners if the order is not passing; in case of minimizing for setter don't need to look for cleaner
-                CleanerData cleanerData;
-                if (!expected.equals(Result.PASS)) {
-                    cleanerData = new CleanerFinder(runner, dependentTest, deps, expected, isolationResult, expectedRun.testOrder()).find();
-                } else {
-                    cleanerData = new CleanerData(dependentTest, expected, isolationResult, new ListEx<CleanerGroup>());
-                }
-
-                polluters.add(new PolluterData(operationTime[0], index, deps, cleanerData));
-
-                // If not configured to find all, since one is found now, can stop looking
-                if (!FIND_ALL) {
-                    break;
-                }
-
-                order.removeAll(deps);  // Look for other deps besides the ones already found
-                index++;
+            } else {
+                final List<String> order = new ArrayList<>(testOrder);
+                getPolluters(order, startTime, polluters, index);
             }
 
             return polluters;
@@ -156,6 +115,71 @@ public class TestMinimizer extends FileCache<MinimizeTestsResult> {
                 return new MinimizeTestsResult(time, expectedRun, expected, dependentTest, polluters, FlakyClass.NOD);
             }
         });
+    }
+
+    private int getPolluters(List<String> order, long startTime, List<PolluterData> polluters, int index) throws Exception {
+        // order can be the prefix + dependentTest or just the prefix. All current uses of this method are using it as just prefix
+        while (!order.isEmpty()) {
+            // First need to check if remaining tests in order still lead to expected value
+            if (result(order) != expected) {
+                info("Remaining tests no longer match expected: " + order);
+                break;
+            }
+
+            final OperationTime[] operationTime = new OperationTime[1];
+            final List<String> deps = OperationTime.runOperation(() -> {
+                return run(new ArrayList<>(order));
+            }, (foundDeps, time) -> {
+                operationTime[0] = time;
+                return foundDeps;
+            });
+
+            if (deps.isEmpty()) {
+                info("Did not find any deps");
+                break;
+            }
+
+            info("Ran minimizer, dependencies: " + deps);
+            double elapsedSeconds = System.currentTimeMillis() / 1000.0 - startTime / 1000.0;
+            if (index == 0) {
+                info("FIRST POLLUTER: Found first polluter " + deps + " for dependent test " + dependentTest + " in " + elapsedSeconds + " seconds.");
+            } else {
+                info("POLLUTER: Found polluter " + deps + " for dependent test " + dependentTest + " in " + elapsedSeconds + " seconds.");
+            }
+
+            // Only look for cleaners if the order is not passing; in case of minimizing for setter don't need to look for cleaner
+            CleanerData cleanerData;
+            if (!expected.equals(Result.PASS)) {
+                cleanerData = new CleanerFinder(runner, dependentTest, deps, expected, isolationResult, expectedRun.testOrder()).find();
+            } else {
+                cleanerData = new CleanerData(dependentTest, expected, isolationResult, new ListEx<CleanerGroup>());
+            }
+
+            polluters.add(new PolluterData(operationTime[0], index, deps, cleanerData));
+	    
+	    // If not configured to find all, since one is found now, can stop looking
+	    if (!FIND_ALL) {
+		break;
+	    }
+
+            // A better implementation would remove one by one and not assume polluter groups are mutually exclusive
+            order.removeAll(deps);  // Look for other deps besides the ones already found
+            index++;
+        }
+        return index;
+    }
+
+    // Returns a list where each element is test from order and the dependent test
+    private List<List<String>> getSingleTests(final List<String> order, String dependentTest) {
+        List<List<String>> singleTests = new ArrayList<>();
+        for (String test : order) {
+            if (test.equalsIgnoreCase(dependentTest)) {
+                continue;
+            }
+            singleTests.add(new ArrayList<>(Collections.singletonList(test)));
+        }
+
+        return singleTests;
     }
 
     private List<String> deltaDebug(final List<String> deps, int n) throws Exception {
