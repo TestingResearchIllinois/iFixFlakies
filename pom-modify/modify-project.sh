6ced0d069a1899449cd736e41aaee39393bd6383
winglam <wing.c.lam@gmail.com>
2020-02-26 15:01:56 -0600

    Adding script and java file to automatically setup the pom files for those using iFixFlakies.

---
+++ pom-modify/modify-project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $1 == "" ]]; then
+    echo "arg1 - the path to the project, where high-level pom.xml is"
+    echo "arg2 - (Optional) Custom version for the artifact (e.g., 1.0.1, 1.0.2-SNAPSHOT)"
+    exit
+fi
+
+if [[ ! $2 == "" ]]; then
+    artifact_vers=$2
+fi
+
+crnt=`pwd`
+working_dir=`dirname $0`
+project_path=$1
+
+cd ${project_path}
+project_path=`pwd`
+cd - > /dev/null
+
+cd ${working_dir}
+
+javac PomFile.java
+find ${project_path} -name pom.xml | grep -v "src/" | java PomFile ${artifact_vers}
+rm -f PomFile.class
+
+cd ${crnt}
