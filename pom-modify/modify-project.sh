b15dbf392bace5a1cce23607ffc656e9fd418933
winglam <wing.c.lam@gmail.com>
2020-02-26 21:34:01 -0600

    Updating readme based on August's feedback.

--- pom-modify/modify-project.sh
+++ pom-modify/modify-project.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DEFAULT_POM="1.0.0-SNAPSHOT"
+
 if [[ $1 == "" ]]; then
     echo "arg1 - the path to the project, where high-level pom.xml is"
-    echo "arg2 - (Optional) Custom version for the artifact (e.g., 1.0.1, 1.0.2-SNAPSHOT)"
+    echo "arg2 - (Optional) Custom version for the artifact (e.g., 1.0.1, 1.0.2-SNAPSHOT). Default is $DEFAULT_POM"
     exit
 fi
 
 if [[ ! $2 == "" ]]; then
-    artifact_vers=$2
+    DEFAULT_POM=$2
 fi
 
 crnt=`pwd`
@@ -21,7 +23,7 @@ cd - > /dev/null
 cd ${working_dir}
 
 javac PomFile.java
-find ${project_path} -name pom.xml | grep -v "src/" | java PomFile ${artifact_vers}
+find ${project_path} -name pom.xml | grep -v "src/" | java PomFile ${DEFAULT_POM}
 rm -f PomFile.class
 
 cd ${crnt}
